c27070a6c56619055ede60d2cae84784669ef534
kaaac <74159167+kaaac@users.noreply.github.com>
2026-04-17 18:34:33 +0200

    add CI workflow and update dependencies for Jest JUnit reporting

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.js'],
   testEnvironment: 'node',
   verbose: true,
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'test-results', outputName: 'junit.xml' }],
+  ],
 };
